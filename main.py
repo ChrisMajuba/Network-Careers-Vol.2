3447def7d4c7e313cafb135b632e23ee9b176daf
ChrisMajuba <124617351+ChrisMajuba@users.noreply.github.com>
2023-03-06 08:28:12 +0000

    Added HTML file and Used Cascading style sheet programming to create a presentable page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
-
+from flask import render_template
+#flask is module that describes the framework used to design web applications in python
+#template means page formatting in this instance
+#Render is the proccess of creating/showing a page in this case
+#The HTML File will contain all the infomation that will be shown
+# CSS Cascading style sheets is a programme used to add style and presentation to a web page 
+# HTML is a programming language used to design and create a web pages
 app = Flask(__name__)
 
 @app.route("/")
 def cyberUniverse():
-  return "Hello CyberUnivere!"
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
